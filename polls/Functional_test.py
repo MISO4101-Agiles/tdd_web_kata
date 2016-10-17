559eec7a470e847e47628efb9bb45d0b4064f2ca
jcastrov <johcavi@gmail.com>
2016-10-17 17:29:46 -0500

    Paso 2 - Test para probar registro (Verde)

--- polls/Functional_test.py
+++ polls/Functional_test.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import os
 
 
 def set_text(self, key, string):
@@ -29,9 +30,6 @@ class FunctionalTest(TestCase):
         set_click(self, 'id_register')
         self.browser.implicitly_wait(2)
 
-        nombre = self.browser.find_element_by_id('id_nombre')
-        nombre.send_keys('Gloria33')
-
         set_text(self, 'id_nombre', 'Gloria')
         set_text(self, 'id_apellidos', 'Cortez')
         set_text(self, 'id_aniosExperiencia', '5')
@@ -39,7 +37,7 @@ class FunctionalTest(TestCase):
             "//select[@id='id_tiposDeServicio']/option[text()='Desarrollador Web']").click()
         set_text(self, 'id_telefono', '3173024578')
         set_text(self, 'id_correo', 'correo@hotmail.com')
-        set_text(self, 'id_imagen', 'C:\\Users\\user\Downloads\\14053863_668055960016699_3847677614404527902_o.jpg')
+        set_text(self, 'id_imagen',  os.path.join(os.getcwd(), 'test_resources', 'piton.jpg'))
         set_text(self, 'id_username', 'gcortes')
         set_text(self, 'id_password', 'clave123')
         set_click(self, 'id_grabar')
