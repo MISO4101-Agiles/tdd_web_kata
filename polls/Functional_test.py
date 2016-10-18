f0ea2e7dbbae4344806babba5f5ce56b0322a1de
jcastrov <johcavi@gmail.com>
2016-10-17 19:37:13 -0500

    Paso 3 - Test para probar Ver detalle (Azul)

--- polls/Functional_test.py
+++ polls/Functional_test.py
@@ -1,66 +1,52 @@
 from unittest import TestCase
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 import os
 
 
-def set_text(self, key, string):
-    nombre = self.browser.find_element_by_id(key)
-    nombre.send_keys(string)
-
-
-def set_click(self, key):
-    link = self.browser.find_element_by_id(key)
-    link.click()
-
-
 class FunctionalTest(TestCase):
+    def set_text(self, key, string):
+        nombre = self.browser.find_element_by_id(key)
+        nombre.send_keys(string)
+
+    def set_click(self, key):
+        link = self.browser.find_element_by_id(key)
+        link.click()
+
+    def assertText(self, xpath, expected):
+        html = self.browser.find_element_by_xpath(xpath)
+        self.assertIn(expected, html.text)
+
     def setUp(self):
         self.browser = webdriver.Firefox()
+        self.browser.get('http://localhost:8000')
 
     def tearDown(self):
         self.browser.quit()
 
     def test_title(self):
-        self.browser.get('http://localhost:8000')
         self.assertIn('BuscoAyuda', self.browser.title)
 
     def test_registro(self):
-        self.browser.get('http://localhost:8000')
-        set_click(self, 'id_register')
+        self.set_click('id_register')
         self.browser.implicitly_wait(2)
-
-        set_text(self, 'id_nombre', 'Gloria')
-        set_text(self, 'id_apellidos', 'Cortez')
-        set_text(self, 'id_aniosExperiencia', '5')
-        self.browser.find_element_by_xpath(
-            "//select[@id='id_tiposDeServicio']/option[text()='Desarrollador Web']").click()
-        set_text(self, 'id_telefono', '3173024578')
-        set_text(self, 'id_correo', 'correo@hotmail.com')
-        set_text(self, 'id_imagen',  os.path.join(os.getcwd(), 'test_resources', 'piton.jpg'))
-        set_text(self, 'id_username', 'gcortes')
-        set_text(self, 'id_password', 'clave123')
-        set_click(self, 'id_grabar')
+        self.set_text('id_nombre', 'Gloria')
+        self.set_text('id_apellidos', 'Cortez')
+        self.set_text('id_aniosExperiencia', '5')
+        tipo_servicio = "//select[@id='id_tiposDeServicio']/option[text()='Desarrollador Web']"
+        self.browser.find_element_by_xpath(tipo_servicio).click()
+        self.set_text('id_telefono', '3173024578')
+        self.set_text('id_correo', 'correo@hotmail.com')
+        self.set_text('id_imagen',  os.path.join(os.getcwd(), 'test_resources', 'piton.jpg'))
+        self.set_text('id_username', 'gcortes')
+        self.set_text('id_password', 'clave123')
+        self.set_click('id_grabar')
         self.browser.implicitly_wait(3)
-        span = self.browser.find_element(By.XPATH, '//span[text()="Gloria Cortez"]')
-
-        self.assertIn("Gloria Cortez", span.text)
+        self.assertText('//span[text()="Gloria Cortez"]', "Gloria Cortez")
 
     def test_detalle(self):
-        self.browser.get('http://localhost:8000')
-        span = self.browser.find_element_by_xpath('//span[text()="Gloria Cortez"]')
-        span.click()
-
+        self.browser.find_element_by_xpath('//span[text()="Gloria Cortez"]').click()
         self.browser.implicitly_wait(2)
-
-        h2 = self.browser.find_element_by_xpath('//h2[text()="Gloria Cortez"]')
-        self.assertIn('Gloria Cortez', h2.text)
-
-        experiencia = self.browser.find_element_by_xpath('//h4[contains(text(),"5 años")]')
-        self.assertIn('5 años', experiencia.text)
-
-        telefono = self.browser.find_element_by_xpath('//h4[contains(text(),"3173024578")]')
-        self.assertIn('3173024578', telefono.text)
-
-        correo = self.browser.find_element_by_xpath('//h4[contains(text(),"correo@hotmail.com")]')
-        self.assertIn('correo@hotmail.com', correo.text)
+        self.assertText('//h2[text()="Gloria Cortez"]', 'Gloria Cortez')
+        self.assertText('//h4[contains(text(),"5 años")]', '5 años')
+        self.assertText('//h4[contains(text(),"3173024578")]', '3173024578')
+        self.assertText('//h4[contains(text(),"correo@hotmail.com")]', 'correo@hotmail.com')
